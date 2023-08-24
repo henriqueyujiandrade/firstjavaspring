c1a376759220a7dbd0ee3a0949e31229b39abe23
henriqueyujiandrade <henrique_yujiandrade@hotmail.com>
2023-08-24 15:46:53 -0300

    feat: up init project

---
+++ src/main/java/hayadev/firstspringjava/FirstspringjavaApplication.java
@@ -0,0 +1,13 @@
+package hayadev.firstspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hayadev/firstspringjava/FirstspringjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package hayadev.firstspringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
